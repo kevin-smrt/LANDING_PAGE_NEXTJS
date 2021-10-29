0877d4e8b62b4966a726d2904fed88a35e412240
kevin_smrt <kevinsanmartin@live.fr>
2021-10-29 13:04:47 +0200

    add theme

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import { ThemeProvider } from "@theme-ui/theme-provider";
 import Layout from "../components/Layout";
+import theme from "../theme";
 
 export default function Home() {
     return (
-        <ThemeProvider>
+        <ThemeProvider theme={theme}>
             <Layout>
             </Layout>
         </ThemeProvider>
