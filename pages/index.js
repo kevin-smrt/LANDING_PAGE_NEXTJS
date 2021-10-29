737bab043b000562cda2abcbdbda970557d9ac6d
kevin_smrt <kevinsanmartin@live.fr>
2021-10-29 21:27:44 +0200

    add Presentation component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@theme-ui/theme-provider";
 import Layout from "../components/Layout";
+import Presentation from "../sections/Presentation";
 import theme from "../theme";
 
 export default function Home() {
     return (
         <ThemeProvider theme={theme}>
             <Layout>
+                <Presentation />
             </Layout>
         </ThemeProvider>
     )
