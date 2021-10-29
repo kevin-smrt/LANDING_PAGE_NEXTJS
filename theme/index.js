747e0926bff17ea5050fa41bd418cbe358325fc7
kevin_smrt <kevinsanmartin@live.fr>
2021-10-29 21:28:00 +0200

    add mmore theme

--- theme/index.js
+++ theme/index.js
@@ -1,5 +1,27 @@
 export default {
     colors: {
         text: '#3b3b3b',
+        primary: '#FF4019',
+        buttonHover: '#B31E00',
+    },
+    buttons: {
+        primary: {
+            cursor: 'pointer',
+            color: 'white',
+            bg: 'primary',
+            '&:hover': {
+                bg: 'buttonHover',
+            }
+        }
+    },
+    images: {
+        plateTwo: {
+            width: '75px',
+            height: 'auto',
+        },
+        plateThree: {
+            width: '80px',
+            height: 'auto',
+        }
     }
 }
