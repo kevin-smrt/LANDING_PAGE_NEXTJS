80ed0628206ca40916fa8eece815419f50461eeb
kevin_smrt <kevinsanmartin@live.fr>
2021-10-29 12:37:35 +0200

    nav link in object

---
+++ components/header/header.data.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        path: 'presentation',
+        label: 'Presentation'
+    },
+
+    {
+        path: 'feature',
+        label: 'Features'
+    },
+
+    {
+        path: 'download',
+        label: 'Download'
+    }
+]
