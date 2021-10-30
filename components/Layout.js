7f7fbfc5ceefdb5f5bab2915ab06ace5ca16b98a
kevin_smrt <kevinsanmartin@live.fr>
2021-10-30 23:33:08 +0200

    add styles on main and sticky

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,6 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import { Fragment, useState } from "react";
 import Sticky from "react-stickynode";
 import Header from "./header/Header";
@@ -15,12 +18,25 @@ export default function Layout(props) {
 
     return (
         <Fragment>
-            <Sticky innerZ={10} top={0} onStateChange={handleStateChange}>
+            <Sticky sx={styles.sticky} innerZ={10} top={0} onStateChange={handleStateChange}>
                 <Header className={`${isSticky ? 'sticky' : 'unSticky'}`} />
             </Sticky>
-            <main>
+            <main sx={styles.main}>
                 {props.children}
             </main>
         </Fragment>
     )
+}
+
+const styles = {
+    sticky: {
+        position: 'absolute',
+        width: '100%',
+        top: '0',
+        left: '0',
+        zIndex: '100',
+    },
+    main: {
+        position: 'relative',
+    }
 }
