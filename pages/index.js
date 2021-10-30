875bf9c2ef98a00f82768cd5e3e796cdbd8da0fa
kevin_smrt <kevinsanmartin@live.fr>
2021-10-30 23:33:57 +0200

    add feature component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import { ThemeProvider } from "@theme-ui/theme-provider";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO/SEO";
 import Presentation from "../sections/Presentation";
+import Features from "../sections/Features";
+
+import { ThemeProvider } from "@theme-ui/theme-provider";
 import theme from "../theme";
 
 export default function Home() {
@@ -10,6 +12,7 @@ export default function Home() {
             <Layout>
                 <SEO />
                 <Presentation />
+                <Features />
             </Layout>
         </ThemeProvider>
     )
