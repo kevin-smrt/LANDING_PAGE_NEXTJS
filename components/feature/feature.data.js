0931c3f536086ed0d86d56d808c219e78c6e95e4
kevin_smrt <kevinsanmartin@live.fr>
2021-11-01 12:04:49 +0100

    add data to feature

---
+++ components/feature/feature.data.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        title: "Best Offer In Town",
+        content: "Lorem ipsum, or lipsum as it's sometimes known, is dummy text used.",
+        imgUrl: "/assets/image-one.png",
+    },
+    {
+        title: "Fast Delivery",
+        content: "Lorem ipsum, or lipsum as it's sometimes known, is dummy text used.",
+        imgUrl: "/assets/image-two.png",
+    },
+    {
+        title: "Quality Food",
+        content: "Lorem ipsum, or lipsum as it's sometimes known, is dummy text used.",
+        imgUrl: "/assets/image-three.png",
+    },
+]
