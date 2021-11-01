2e5e4b3f3d06f8da52cc8ea2a9942910b46594d5
kevin_smrt <kevinsanmartin@live.fr>
2021-11-01 12:06:22 +0100

    add Features section

--- sections/Features.js
+++ sections/Features.js
@@ -1,20 +1,39 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
+import { jsx, Flex, Divider, Container } from 'theme-ui';
+import Feature from '../components/feature/Feature';
+import featureItems from '../components/feature/feature.data';
 
 export default function Features() {
     return (
-        <section sx={styles.oui}>
-            <h1>Features</h1>
+        <section sx={styles.features} id="feature">
+            <Container sx={styles.container}>
+                <Flex sx={styles.flex}>
+                    {featureItems.map((featureItem, index) => (
+                        <Feature
+                            key={index}
+                            title={featureItem.title}
+                            content={featureItem.content}
+                            imgUrl={featureItem.imgUrl}
+                        />
+                    ))}
+                </Flex>
+                <Divider />
+            </Container>
         </section>
     )
 }
 
 const styles = {
-    oui: {
+    features: {
         position: 'relative',
         mt: '100vh',
-        height: '100vh',
         bg: 'whitesmoke',
-    }
+    },
+    container: {
+        width: '60%',
+    },
+    flex: {
+        justifyContent: 'center',
+    },
 }
