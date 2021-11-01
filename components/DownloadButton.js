4be0cdc2b07a7a42f6c77d033b3e07b06d698a81
kevin_smrt <kevinsanmartin@live.fr>
2021-11-01 18:02:22 +0100

    add component for download button

---
+++ components/DownloadButton.js
@@ -0,0 +1,28 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Container, Text, Flex, Button, Image, Box } from 'theme-ui';
+
+export default function DownloadButton(props) {
+    return (
+        <Button variant="download">
+            <Flex>
+                <Image sx={styles.img} src={props.imgUrl} />
+                <Box>
+                    <Text as="p">{props.text}</Text>
+                    <span sx={styles.button}>{props.platform}</span>
+                </Box>
+            </Flex>
+        </Button>
+    )
+}
+
+const styles = {
+    button: {
+        display: 'block',
+        fontSize: 24,
+        fontWeight: 500,
+    },
+    img: {
+        objectFit: 'contain',
+    }
+}
