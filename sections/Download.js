c99069f7bcebbbd84eb6377675daaec9a2f55819
kevin_smrt <kevinsanmartin@live.fr>
2021-11-01 18:04:45 +0100

    add download section

---
+++ sections/Download.js
@@ -0,0 +1,46 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Container, Text, Flex, Button, Image, Box } from 'theme-ui';
+import DownloadButton from '../components/DownloadButton';
+
+export default function Download() {
+    return (
+        <section sx={styles.download} id="download">
+            <Container sx={styles.container}>
+                <Text as="h2">Download The App</Text>
+                <Text as="h3" sx={styles.subtitle}>Get Food With Foodo</Text>
+                <Text as="p">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Text>
+                <Flex sx={styles.flex}>
+                    <DownloadButton
+                        imgUrl="/assets/ios-icon.png"
+                        text="Download On The"
+                        platform="App Store"
+                    />
+                    <DownloadButton
+                        imgUrl="/assets/google-icon.png"
+                        text="Android App On"
+                        platform="Google play"
+                    />
+                </Flex>
+            </Container>
+        </section>
+    )
+}
+
+const styles = {
+    download: {
+        position: 'relative',
+        bg: 'secondary',
+        py: 5,
+    },
+    container: {
+        width: '60%',
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontWeight: 500,
+    },
+    flex: {
+        justifyContent: 'center',
+    }
+}
